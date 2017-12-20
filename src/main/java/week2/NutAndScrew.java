9f0a68d9a0e0acc3c820f9e8d3eb2b0c84dde41b
lujxu <329088775@qq.com>
2017-12-21 01:39:12 +0800

    week2

--- src/main/java/week2/NutAndScrew.java
+++ src/main/java/week2/NutAndScrew.java
@@ -1,7 +1,71 @@
 package week2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
+ * 螺丝和螺母匹配问题
  * Created by lujxu on 2017/12/20.
  */
 public class NutAndScrew {
+
+    Map<Integer, Integer> map=new HashMap<>();
+
+    public static void main(String []args) {
+        int[] nuts = new int[]{4, 2,  1, 7,  9, 6, 5, 3};
+        int[] screws = new int[]{2,  3, 7, 4, 1,  9, 6, 5};
+        NutAndScrew n=new NutAndScrew();
+        n.sort(nuts, screws, 0, nuts.length-1);
+        Map<Integer, Integer> map=n.map;
+        System.out.println("test");
+    }
+
+    public void sort(int []nuts, int [] screws, int start, int end){
+        if (start>end)
+            return;
+        if (start==end){
+                map.put(nuts[start],screws[start]);
+                return;
+        }
+        int nut=nuts[start];
+        int s=partition(screws, start, end, nut);
+        partition(nuts, start, end, screws[s]);
+        map.put(nut, screws[s]);
+        sort(nuts, screws, start, s-1);
+        sort(nuts, screws, s+1, end);
+    }
+
+    public int partition(int []  list, int start, int end, int pivot){
+        if (start>=end)
+            return 0;
+        int [] array=list.clone();
+        int s=0;
+        int i=0;
+        int j=0;
+        for (int k=0;k<=end-start; k++){
+            if (array[start+k]<pivot){
+                list[i+start]=array[start+k];
+                i++;
+            }else if (array[start+k]>pivot){
+                list[end-j]=array[start+k];
+                j++;
+            }else{
+                s=array[start+k];
+            }
+        }
+        list[i+start]=s;
+        return i+start;
+    }
+
+    public void swap(int []  list, int i, int j){
+        if (list==null||list.length<j||list.length<i)
+            return;
+        int temp=list[i];
+        list[i]=list[j];
+        list[j]=temp;
+        return;
+    }
+
 }
--- src/main/java/week2/QuickSort.java
+++ src/main/java/week2/QuickSort.java
@@ -1,10 +1,11 @@
 package week2;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 /**
+ * 使用递归和非递归的方式实现快速排序
  * Created by lujxu on 2017/12/18.
  */
 public class QuickSort {
@@ -16,7 +17,9 @@ public class QuickSort {
             list.add(i);
         }
         QuickSort q=new QuickSort();
-        System.out.println(q.quickSort(array,0, array.length-1));
+       // System.out.println(q.quickSort(array,0, array.length-1));
+       // q.getPivot(new int[]{5,5},0,1);
+        System.out.println(q.quickSort(array));
         System.out.println("test");
     }
 
@@ -36,16 +39,22 @@ public class QuickSort {
         int s=list[start];
         int i=start;
         int j=end;
-        while (i<=j){
-            while (list[i]<=s&&i<=j)
-                i++;
-            while (list[j]>=s&&i<=j)
+        while (i!=j){
+            while (i<j&&list[j]>s)
                 j--;
-            swap(list,i,j);
+            if (i<j){
+                list[i]=list[j];
+                i++;
+            }
+            while (i<j&&list[i]<s)
+                i++;
+           if (i<j){
+               list[j]=list[i];
+               j--;
+           }
         }
-        swap(list,i,j);
-        swap(list,j,start);
-        return j;
+        list[i]=s;
+        return i;
     }
 
     public void swap(int []  list, int i, int j){
@@ -55,5 +64,39 @@ public class QuickSort {
         list[i]=list[j];
         list[j]=temp;
         return;
+    }
+
+    public int[] quickSort(int [] list){
+        Stack<int []> stack=new Stack<>();
+        stack.push(list);
+        int index=0;
+        while (!stack.isEmpty()){
+            int [] array=stack.pop();
+            if (array.length==0)
+                continue;
+            if (array.length==1){
+                list[index++]=array[0];
+                continue;
+            }
+            int pivot=getPivot(array,0,array.length-1);
+            int [] a=null;
+            int [] b=null;
+            if (pivot!=0)
+                a=new int[pivot];
+            if (array.length-1!=pivot)
+                b=new int[array.length-1-pivot];
+            for (int i=0;a!=null&&i<a.length;i++){
+                a[i]=array[i];
+            }
+            for (int i=0;b!=null&&i<b.length;i++){
+                b[i]=array[pivot+1+i];
+            }
+            if (b!=null)
+                stack.push(b);
+            stack.push(new int[]{array[pivot]});
+            if (a!=null)
+                stack.push(a);
+        }
+        return list;
     }
 }
